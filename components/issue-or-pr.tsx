2f77d20ea7d33a7bf9c51e538bf5d28adb210996
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2023-12-05 12:19:48 -0500

    Fix TypeError in alert icon

--- components/issue-or-pr.tsx
+++ components/issue-or-pr.tsx
@@ -57,10 +57,10 @@ export function IssueOrPR() {
   );
 }
 
-function IconAlertcircle(props) {
+function IconAlertcircle({ className }: { className: string }) {
   return (
     <svg
-      {...props}
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
