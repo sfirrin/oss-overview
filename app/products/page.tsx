e22044e5301a007e9a1e5ca6b951046b061ea081
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2024-04-25 10:23:50 -0400

    Read cookies to make /products dynamic render

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,13 @@
+import { cookies } from 'next/headers'
 
 
 export default function Page () {
+    const cookieStore = cookies();
     const productCount = (undefined as any).products.length;
 
     return (
         <main className="mt-3">
-            HEllo World {productCount}
+            HEllo World {productCount} {cookieStore.get('auth_token')}
         </main>
     )
 }
