005f75ba66dd27a40e7b1da27a8a2f3d376eb4c0
Stephen Firincieli <stephen.firrincieli@datadoghq.com>
2024-12-10 10:02:20 -0700

    Add otel registration log

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,7 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
+  console.log("Registering OpenTelemetry...");
   registerOTel({ serviceName: "oss-overview" });
 }
 // NOTE: You can replace `your-project-name` with the actual name of your project
